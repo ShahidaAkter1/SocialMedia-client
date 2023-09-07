e86675d4b9fc35c7823ec09f07bd8a770e9b9a64
ShahidaAkter1 <shahida0200@gmail.com>
2023-09-07 12:03:06 +0600

    initial setup

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Home.css'
+
+const Home = () => {
+    return (
+        <div className='Home'>
+            <div className="profileSide">Profile</div>
+            <div className="postSide">Post</div>
+            <div className="RightSide">RightSide</div>
+        </div>
+    );
+};
+
+export default Home;
