786dd2b769e9f184ab4e2b62b7e45cb60d42e6e3
ShahidaAkter1 <shahida0200@gmail.com>
2023-09-09 18:25:52 +0600

    followersCard added

---
+++ src/Data/FollowersData.js
@@ -0,0 +1,11 @@
+import img1 from '../img/img1.png';
+import img2 from '../img/img2.png';
+import img3 from '../img/img3.png';
+import img4 from '../img/img4.jpg';
+
+export const Followers = [
+    {name:"Jewel Rana", username:"JewelRana" , img:img1},
+    {name:"Suad Rana", username:"Suad Rana" , img:img2},
+    {name:"Shahida Akter", username:"shahidaAkter" , img:img3},
+    {name:"Sumi Akter", username:"sumiAkter" , img:img4},
+]
