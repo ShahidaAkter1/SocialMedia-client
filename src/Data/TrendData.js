7a7a4eb5e65efcdfdd971947d4ec549dbc4c66b3
ShahidaAkter1 <shahida0200@gmail.com>
2023-09-20 20:23:38 +0600

    Trend Card

---
+++ src/Data/TrendData.js
@@ -0,0 +1,26 @@
+export const TrendData = [
+    {
+        name: "Minions",
+        shares:97,
+    },
+    {
+        name: "Aveangers",
+        shares:80.4,
+    },
+    {
+        name: "Zainkeepscode",
+        shares:75.5,
+    },
+    {
+        name: "ReactJs",
+        shares:72,
+    },
+    {
+        name: "Elon Musk",
+        shares:92,
+    },
+    {
+        name: "Need for speed",
+        shares:20,
+    },
+]
