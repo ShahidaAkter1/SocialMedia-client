6ef5a2e74b6eb9af76ab9eb3bd61c9a7ad57c526
ShahidaAkter1 <shahida0200@gmail.com>
2023-09-20 19:46:48 +0600

    Posts section added

---
+++ src/Data/PostsData.js
@@ -0,0 +1,28 @@
+import postPic1 from '../img/postpic1.jpg';
+import postPic2 from '../img/postpic2.jpg';
+import postPic3 from '../img/postpic3.JPG';
+
+export const PostsData = [
+    {
+        img: postPic1,
+        name:'Shahida',
+        desc:'Happy New Year all friends #2023',
+        likes:2300,
+        liked:true
+    },
+    {
+        img: postPic2,
+        name:'Suad',
+        desc:'Happy New Party time all friends #2023',
+        likes:2300,
+        liked:false
+    },
+    {
+        img: postPic2,
+        name:'Jewel',
+        desc:'Happy Birthday',
+        likes:800,
+        liked:false
+    },
+]
+
